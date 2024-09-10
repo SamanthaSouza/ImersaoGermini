7678026fcf464994f90497cd5b807d8dfb665703
SamanthaSouza <73179503+SamanthaSouza@users.noreply.github.com>
2024-09-09 23:27:21 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,68 @@
+let dados = [
+    {
+        titulo : "Wicked", 
+        descricao : "Wicked é um musical da Broadway que reimagina a história de O Mágico de Oz, contada sob a perspectiva da bruxa verde, Elphaba. A peça explora a amizade complexa entre Elphaba e Glinda, a boa bruxa, e revela os eventos que levaram à formação de suas personalidades e destinos.",
+        link : "https://pt.wikipedia.org/wiki/Wicked_(filme)",
+        tags : "musical fantasia broadway"
+    },
+    {
+        titulo : "Les Miserables", 
+        descricao : "Os Miseráveis é uma adaptação cinematográfica do aclamado musical da Broadway, inspirado na obra de Victor Hugo. Ambientado na França do século XIX, o filme conta a história de Jean Valjean, um ex-condenado em busca de redenção, e sua perseguição incansável pelo inspetor Javert.",
+        link : "https://pt.wikipedia.org/wiki/Os_Miser%C3%A1veis",
+        tags: "musical fantasia broadway"
+    },
+    {
+        titulo : "Corpse Bride", 
+        descricao : "Corpse Bride é uma animação em stop-motion da Laika que nos leva a um mundo fantasioso e sombrio. A história gira em torno de Victor, um jovem nervoso que, enquanto ensaiva seus votos de casamento, coloca acidentalmente uma aliança em um dedo esquelético que emerge do chão. Ele acaba se casando com Emily, uma noiva morta há muito tempo, que o arrasta para o mundo dos mortos.",
+        link : "https://pt.wikipedia.org/wiki/Corpse_Bride",
+        tags: "animação fantasia gótico"
+    },
+    {
+        titulo: "Cabaret",
+        descricao: "Cabaret é um musical que retrata a vida em Berlim durante a ascensão do nazismo, através da perspectiva dos artistas que se apresentavam em um clube noturno. O filme explora temas como decadência, amor e a ameaça do fascismo.",
+        link: "https://pt.wikipedia.org/wiki/Cabaret_(filme)",
+        tags: "musical drama nazismo"
+    },
+    {
+        titulo: "Coraline",
+        descricao: "Coraline é um filme de animação em stop-motion que conta a história de uma menina que descobre uma porta secreta que a leva para um mundo paralelo, onde tudo parece perfeito, mas esconde um segredo sinistro.",
+        link: "https://pt.wikipedia.org/wiki/Coraline_(filme)",
+        tags: "animação fantasia mundos paralelos"
+    },
+    {
+        titulo: "A Noviça Rebelde",
+        descricao: "A Noviça Rebelde é um musical clássico que conta a história de uma jovem noviça austríaca que é enviada para cuidar de sete crianças órfãs. Através da música e do amor, ela transforma a vida da família e da comunidade.",
+        link: "https://pt.wikipedia.org/wiki/The_Sound_of_Music",
+        tags: "musical drama familiar Segunda Guerra Mundial"
+    },
+    {
+        titulo: "Singing in the Rain",
+        descricao: "Singing in the Rain é um musical clássico que retrata a transição do cinema mudo para o cinema falado. O filme é uma homenagem ao cinema e à música, com cenas icônicas como a dança na chuva.",
+        link: "https://pt.wikipedia.org/wiki/Singin'_in_the_Rain",
+        tags: "musical comédia Hollywood"
+    },
+    {
+        titulo: "O Poderoso Chefão",
+        descricao: "Um marco do cinema, nos imerge no mundo da máfia italiana, explorando temas como família, lealdade e poder. A trama, centrada na família Corleone, nos apresenta um retrato complexo e fascinante do crime organizado.",
+        link: "https://pt.wikipedia.org/wiki/O_Poderoso_Chefão",
+        tags: "drama máfia Itália"
+    },
+    {
+        titulo: "O Senhor dos Anéis: A Sociedade do Anel",
+        descricao: "Nos transporta para a Terra Média, um mundo de fantasia épica. A jornada de Frodo Bolson para destruir o Um Anel é um clássico da literatura adaptada para o cinema, repleto de aventura, amizade e a luta entre o bem e o mal.",
+        link: "https://pt.wikipedia.org/wiki/O_Senhor_dos_Anéis:_A_Sociedade_do_Anel",
+        tags: "fantasia aventura magia"
+    },
+    {
+        titulo: "Pulp Fiction",
+        descricao: "É uma obra-prima do cinema pós-moderno, que quebra as convenções narrativas com uma trama não linear e personagens excêntricos. O filme de Quentin Tarantino é uma celebração da cultura pop, com diálogos memoráveis e cenas icônicas.",
+        link: "https://pt.wikipedia.org/wiki/Pulp_Fiction",
+        tags: "crime drama violência"
+    },
+    {
+        titulo: "O Iluminado",
+        descricao: "Um escritor, sua esposa e seu filho pequeno se mudam para um isolado hotel de montanha no inverno, onde eventos sobrenaturais começam a acontecer.",
+        link: "https://pt.wikipedia.org/wiki/O_Iluminado",
+        tags: "terror psicológico isolamento"
+    }
+];
